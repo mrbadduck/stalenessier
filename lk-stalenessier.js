932b66b320733b6e4b98c25553095c850e7d2d74
mrbadduck <eric.mirowitz@gmail.com>
2017-05-01 09:20:38 -0500

    add int parse on increment, clean up error messages
    also converted exit(1)’s into returns and put the whole shebang in one
    function so return will exit gracefully after spitting the error
    message to console.

--- lk-stalenessier.js
+++ lk-stalenessier.js
@@ -2,6 +2,7 @@
 var LeanKitClient = require( "leankit-client" );
 var read = require('read')
 
+
 // Update this variable to the name of your LeanKit 
 // account, which can be found in the URL you use 
 // to access your LeanKit boards. For example, 
@@ -32,77 +33,85 @@ if (process.argv.length > 2) {
 
 	boardId = process.argv[2];
 	laneTitle = process.argv[3];
-	increment = process.argv[4];
-}
+	increment = parseInt(process.argv[4]);
 
-read({ prompt: 'Login E-mail: '}, function(er, email) {
-read({ prompt: 'Password (silent): ', silent: true }, function(er, password) {
-  
-	// Create an instance of the client with your credentials
-	var client = new LeanKitClient( accountName, email, password );
+	if (!increment) {
+		console.error( "Invalid increment value.");
+		return;
+	}
 
-	client.getBoard( boardId, function( err, board ) {  
-	    if ( err ) console.error( "Error getting board:", boardId, err );
+	read({ prompt: 'Login E-mail: '}, function(er, email) {
+	read({ prompt: 'Password (silent): ', silent: true }, function(er, password) {
+	  
+		// Create an instance of the client with your credentials
+		var client = new LeanKitClient( accountName, email, password );
 
-	    // Get the active lanes from the board
-	    var lanes = board.Lanes;
+		client.getBoard( boardId, function( err, board ) {  
+		    if ( err ) {
+		    	console.error( "Error getting board: %s. Double check the board ID and login credentials.", boardId);
+		    	return;
+		    }
 
-	    // Loop through the lanes to find the lane of interest
-	    var targetLane = null;
-	    for (var i = 0; i < lanes.length; i++) {
-	    	//console.log(lanes[i].Title);
-	    	if ( lanes[i].Title == laneTitle ) {
-	    		targetLane = lanes[i];
-	    		break;
-	    	}
-	    }
+		    // Get the active lanes from the board
+		    var lanes = board.Lanes;
 
-	    // If you didn't find the lane, abort!
-	    if ( targetLane == null ) {
-	    	console.error( "Could not find lane:", laneTitle);
-	    	exit(1);
-	    }
+		    // Loop through the lanes to find the lane of interest
+		    var targetLane = null;
+		    for (var i = 0; i < lanes.length; i++) {
+		    	//console.log(lanes[i].Title);
+		    	if ( lanes[i].Title == laneTitle ) {
+		    		targetLane = lanes[i];
+		    		break;
+		    	}
+		    }
 
-	    // Loop through the board users to find the user who made this request
-	    var user = null;
-	    for (var i = 0; i < board.BoardUsers.length; i++) {
-	    	//console.log(board.BoardUsers[i].EmailAddress);
-	    	if ( board.BoardUsers[i].EmailAddress == email ) {
-	    		user = board.BoardUsers[i];
-	    		break;
-	    	}
-	    }
-	    
-	    // If you didn't find the user, abort!
-	    if ( user == null ) {
-	    	console.error( "Could not find the user on the board:", email);
-	    	exit(1);
-	    }
+		    // If you didn't find the lane, abort!
+		    if ( targetLane == null ) {
+		    	console.error( "Could not find lane:", laneTitle);
+		    	return;
+		    }
 
-	    // Get all the cards in the target lane
-	    var cards = targetLane.Cards;
-
-	    function stalenessifyCards( i ) {
-		    // Loop through the cards and increment their size by the increment amount
-		    var card = cards[ i ];
+		    // Loop through the board users to find the user who made this request
+		    var user = null;
+		    for (var i = 0; i < board.BoardUsers.length; i++) {
+		    	//console.log(board.BoardUsers[i].EmailAddress);
+		    	if ( board.BoardUsers[i].EmailAddress == email ) {
+		    		user = board.BoardUsers[i];
+		    		break;
+		    	}
+		    }
 		    
-		    if ( !card ) return;
-		        
-		    client.updateCardFields( {CardId: card.Id, Size: (card.Size + increment)}, function (err, res) {
-		        if (err) console.error ("Error updating card:", card.Id, err);
-		        else console.log((i + 1) + '/' + cards.length, card.Id, '"' + card.Title + '"', "Size Updated", card.Size, card.Size + increment);
+		    // If you didn't find the user, abort!
+		    if ( user == null ) {
+		    	console.error( "Could not find the user on the board:", email);
+		    	return;
+		    }
 
-		        client.addComment( boardId, card.Id, user.Id, "Size updated for staleness to " + (card.Size + increment), function (err, res) {
-		            if (err) console.error ("Error commenting on card:", card.Id, err);
-		        
-		        	stalenessifyCards( i + 1 );
-		        } );
-		    } );
-		} 
+		    // Get all the cards in the target lane
+		    var cards = targetLane.Cards;
 
-	    // Get all the cards in the target lane
-	    stalenessifyCards( 0 );
-	} );
-} )
-} )
+		    function stalenessifyCards( i ) {
+			    // Loop through the cards and increment their size by the increment amount
+			    var card = cards[ i ];
+			    
+			    if ( !card ) return;
+			        
+			    client.updateCardFields( {CardId: card.Id, Size: (card.Size + increment)}, function (err, res) {
+			        if (err) console.error ("Error updating card:", card.Id, err);
+			        else console.log((i + 1) + '/' + cards.length, card.Id, '"' + card.Title + '"', "Size Updated", card.Size, card.Size + increment);
 
+			        client.addComment( boardId, card.Id, user.Id, "Size updated for staleness to " + (card.Size + increment), function (err, res) {
+			            if (err) console.error ("Error commenting on card:", card.Id, err);
+			        
+			        	stalenessifyCards( i + 1 );
+			        } );
+			    } );
+			} 
+
+		    // Get all the cards in the target lane
+		    stalenessifyCards( 0 );
+		} );
+	} )
+	} )
+
+}
